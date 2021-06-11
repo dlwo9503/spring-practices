04e4c83d3f63a57bd08e5e4c9ba4de434a807abf
dlwo9503 <dlwo9503@naver.com>
2021-06-11 11:47:56 +0900

    wiring #1

--- fileupload/src/main/java/com/douzone/fileupload/controller/FileUploadController.java
+++ fileupload/src/main/java/com/douzone/fileupload/controller/FileUploadController.java
@@ -1,13 +1,19 @@
 package com.douzone.fileupload.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.douzone.fileupload.service.FileUploadService;
+
 @Controller
 public class FileUploadController {
+	@Autowired
+	private FileUploadService fileUploadService;
 	
 	@RequestMapping({"", "/form"})
 	public String form() {
@@ -16,7 +22,13 @@ public class FileUploadController {
 	
 	@RequestMapping(value="/upload", method=RequestMethod.POST)
 	public String upload(
-			@RequestParam("file1") MultipartFile file1) { // [] s 이런식으로 하려면 file이름을 모두 같게 해줘야 함
+		@RequestParam(value="email", required=true, defaultValue="") String email,	
+		@RequestParam("file1") MultipartFile file1,
+		Model model) {
+		System.out.println("email:" + email);
+		
+		String url = fileUploadService.restore(file1);
+		model.addAttribute("url", url);
 		return "/WEB-INF/views/result.jsp";
 	}
 }
